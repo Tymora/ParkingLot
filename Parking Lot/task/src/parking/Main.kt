57a1a069629fa79b69af7260575aed34c1e02571
Tymora <dachabel@mail.ru>
2023-12-09 00:37:58 +0300

    Edit

--- Parking Lot/task/src/parking/Main.kt
+++ Parking Lot/task/src/parking/Main.kt
@@ -1,42 +1,41 @@
 package parking
 
+var spot1: Car? = null
+var spot2: Car? = null
+
 fun main() {
     val lot = Parking()
-    val input1 = readln()
-    while (true) {
-        lot.park()
+    var parsing = mutableListOf<String>()
+    parsing = readln().split(" ").toMutableList()
+    when (parsing[0]) {
+        "park" -> lot.park()
+        "leave" -> lot.unpark()
     }
-
 }
 
-class Parking() {
-    var spot1 = mutableListOf<String>()
-    var spot2 = mutableListOf<String>()
+class Parking(var parsing: MutableList<String>) {
+    fun get_spot() {
+        if (spot1 == null) {
+            // Как это сделать блин?
+        }
+    }
 
     fun park() {
-        if (spot1.isEmpty()) {
-            spot1 = readln().split(" ").toMutableList()
-            println("${spot1[2]} car parked in spot 1")
-        } else if (spot2.isEmpty()) {
-            spot2 = readln().split(" ").toMutableList()
-            println("${spot2[2]} car parked in spot 2")
-        } else println("Not free spots")
+
+//        if (spot1? == null){
+//            spot1 = Car(parsing[1], parsing[2])
+//        } else if (spot2? == null){
+//            spot2 = Car(parsing[1], parsing[2])
+//        } else println("Not free spots")
     }
 
-    fun unpark(input1: String) {
-        when (input1) {
-            "leave 1" -> if (spot1.isNotEmpty()) {
-                spot1.clear()
-            } else {
-                println("There is no car in spot 1.")
-            }
-            "leave 2" -> if (spot2.isNotEmpty()) {
-                spot2.clear()
-            } else {
-                println("There is no car in spot 2.")
-            }
+    fun unpark(parsing: MutableList<String>) {
+        when (parsing[1]) {
+            "1" -> spot1 = null
+            "2" -> spot1 = null
         }
-
+        println("Spot ${parsing[1]} is free.")
     }
-
 }
+
+data class Car(val reg_number: String?, val color: String?)
